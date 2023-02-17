433f168e33f96a866dd0d49491e87c5c06c3d351
kennedy mwangi <kennedymwangi200083@gmail.com>
2023-02-17 10:28:17 -0800

    betty code style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-int main()
+/*
+ * main=where the main function is and code
+ * puts()function=allows printing of string as it is
+ */
+int main(void)
 {
-	char Code[] = "\"Programming is like building a multilingual puzzle";
-	puts(Code);
-	return 0;
+char Code[] = "\"Programming is like building a multilingual puzzle";
+puts(Code);
+return (0);
 }
