cad701369ad92943bad55eac3fc4cbe49f58acac
kennedy mwangi <kennedymwangi200083@gmail.com>
2023-02-17 09:44:44 -0800

    writing a program that prints the string only using puts()function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <string.h>
 int main()
 {
-	char Code[]="Programming is like building a multilingual puzzle";
-	puts(Code);
-	return 0;
+	char Code[] = "Programming is like building a multilingual puzzle";
+puts(Code);
+return 0;
 }
