f1d6f2541f6b97d8705e8d90ef508e5d4ccd8a1b
kennedy mwangi <kennedymwangi200083@gmail.com>
2023-02-20 08:33:50 -0800

    use for statement

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main entry of code
+ * Return: 0 means is successful
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	putchar(alphabet);
+	putchar('\n');
+}
+return (0);
+	}
