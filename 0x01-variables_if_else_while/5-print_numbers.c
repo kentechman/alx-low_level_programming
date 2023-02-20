84f0d21e9c1498f6790c3015684f4631215c38ef
kennedy mwangi <kennedymwangi200083@gmail.com>
2023-02-20 11:14:34 -0800

    numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main code entry
+ * Return: 0 is saccess
+ */
+int main(void)
+{
+int base;
+
+for (base = 0; base <= 9; base++)
+{
+printf("%d\n", base);
+}
+return (0);
+}
