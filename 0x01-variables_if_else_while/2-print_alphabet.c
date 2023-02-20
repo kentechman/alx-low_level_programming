33d732dd393a71d6fbcb397bbf1f145f92aa8a45
kennedy mwangi <kennedymwangi200083@gmail.com>
2023-02-20 08:59:26 -0800

    use of for statement arguement

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,11 @@
  */
 int main(void)
 {
-	char alphabet;
+char alp;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+for (alp = 'a'; alp <= 'z'; alp++)
 {
-	putchar(alphabet);
-	
+putchar(alp);
 }
 return (0);
-	}
+}
