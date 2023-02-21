edcaf1adcd94c4d32f04ba30138926df521dd719
kennedy mwangi <kennedymwangi200083@gmail.com>
2023-02-21 10:48:43 -0800

    alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabets 10 times
+ * Return: 0 is for success
+ */
+void print_alphabet_x10(void)
+{
+	char alp;
+	int i = 0;
+
+	while (i <= 9)
+	{
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+	_putchar(alp);
+	_putchar('\n');
+	i++;
+	}
+	}
+}
