d105a9580f1fee69ec7d9fd336922a5f485cf957
kennedy mwangi <kennedymwangi200083@gmail.com>
2023-02-21 10:01:45 -0800

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - main entry
+ * Return: 0 is success
+ */
+void print_alphabet(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		_putchar(alp);
+	}
+	_putchar('\n');
+}
