0344a4471076660cf59121ed838c30882eee9abe
kennedy mwangi <kennedymwangi200083@gmail.com>
2023-02-23 09:25:26 -0800

    debug

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+ * main - main entry of source code
+ * Return: 0 is success
+ */
+int main(void)
+{
+int a;
+int b;
+
+for (a = 0; a <= 9; a++)
+{
+for (b = a + 1; b <= 9; b++)
+}
+if (a !=b)
+{
+putchar(a);
+putchar(b);
+{
+if (a == '8' && b == '9')
+{
+continue;
+{
+{
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
