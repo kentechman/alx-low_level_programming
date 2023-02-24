d26e46527ea9fa6ca508c31b24f85ad148424b6e
kennedy mwangi <kennedymwangi200083@gmail.com>
2023-02-24 09:28:39 -0800

    printing of numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,16 @@ void more_numbers(void)
 int x;
 int y;
 
-y = 0;
-while (y <= 9)
+for ( x = 0; x < 10; x++)
 {
-for (x = 0; x <= 14; x++)
+for (y = 0; y <= 14; y++)
 {
-_putchar(x + '0');
+if (y > 9)
+{
+_putchar((y / 10) + '0');
+}
+_putchar((y % 10) + '0');
 }
 _putchar('\n');
-y++;
 }
 }
