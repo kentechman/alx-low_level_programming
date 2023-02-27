bc6993424e0c1f85072959ef6d852843c2fa1cc9
kennedy mwangi <kennedymwangi200083@gmail.com>
2023-02-27 09:32:50 -0800

    printing of stringns

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by a new line
+ * @str: the string to be printed
+ * Return: always 0
+ */
+void _puts(char *str)
+{
+	_putchar(*str);
+	_putchar('\n');
+	return (0);
+}
