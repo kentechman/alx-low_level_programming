3a3641c6d1480cc594c845332f09b02f901b9c0e
kennedy mwangi <kennedymwangi200083@gmail.com>
2023-02-27 08:24:20 -0800

    assigning a value to a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - updating the value of variable in a pointer
+ * @n: the variable being asigned a value
+ * Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
