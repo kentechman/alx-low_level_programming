b14c5c8db005a15a40a3a4c62546b2dec70a9182
kennedy mwangi <kennedymwangi200083@gmail.com>
2023-02-27 09:12:13 -0800

    lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - the lenght os the string
+ * @s: the string lengt
+ * Return: return string length
+ */
+int _strlen(char *s)
+{
+int length;
+length = strlen(s);
+return (length);
+}
